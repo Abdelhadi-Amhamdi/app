e19a8fea543ca99c80ffe6a135475df1e620d313
Abdelhadi Amhamdi <aamhamdi943@gmail.com>
2024-10-23 17:48:35 +0100

    [add] create offer action

--- application/app/lib/actions.ts
+++ application/app/lib/actions.ts
@@ -1,8 +1,8 @@
 'use server'
 
 import { redirect } from "next/navigation"
-
-
+import jwt  from 'jsonwebtoken'
+import { cookies } from "next/headers"
 
 const connectionPool = require('../../db')
 
@@ -33,12 +33,44 @@ export async function loguser(formData : FormData) {
         password : formData.get('password')
     }
 
-    const user = await connectionPool.query(`
+    const data = await connectionPool.query(`
         SELECT * FROM users WHERE users.email = $1 AND users.password = $2
     `, [rawFormData.email, rawFormData.password])
 
-    if(user.rows) {
+
+    if(data.rows.length > 0) {
+        const user = data.rows[0]
         // create a jwt token and set cookies
-        redirect('/dashboard')
+        const token = jwt.sign(
+            {id : user.id,},
+            "secret",
+            { expiresIn: '1h' }
+        )
+        
+        ;(await cookies()).set('token', token, {
+            httpOnly: true,
+            maxAge : 60 * 60,
+            path : '/',
+            sameSite : 'strict'
+        })
+       redirect('/dashboard')
     }
+}
+
+export async function createOffer(formData : FormData) {
+    const rawFormData = {
+        type : formData.get('type'),
+        title : formData.get('title'),
+        description : formData.get('description')
+    }
+
+    const token = (await cookies()).get('token')
+    const { id } = jwt.verify(token?.value, "secret")
+
+    await connectionPool.query(`
+        INSERT INTO offers (title, description, type, user_id, status)
+        VALUES ($1, $2, $3, $4, $5)
+    `, [rawFormData.title, rawFormData.description, rawFormData.type, id ,"open"])
+    
+    redirect('/dashboard/offers')
 }
